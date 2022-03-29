92ecf91d5ea360707829b961a5e16f17384d93a4
Mark Grushevski <markgrushevski@gmail.com>
2022-03-29 19:13:23 +0500

    first commit

---
+++ 5/index.js
@@ -0,0 +1,25 @@
+"use strict";
+let images = ["1.jpg", "2.jpg", "3.jpg"];
+
+window.addEventListener("load", function () {
+    let gallery = document.querySelector(".gallery");
+    let selected = document.querySelector(".selected");
+
+    selected.insertAdjacentHTML(
+        "beforeend",
+        `<img alt="pic" id="selected__img" src="./assets/${images[0]}">`
+    );
+
+    let selectedImg = document.getElementById("selected__img");
+
+    images.forEach((src, i) => {
+        gallery.insertAdjacentHTML(
+            "beforeend",
+            `<img alt="${i}" class="gallery__img" src="./assets/${src}">`
+        );
+
+        gallery.children[i].addEventListener("click", function (ev) {
+            selectedImg.src = this.src;
+        });
+    });
+});
